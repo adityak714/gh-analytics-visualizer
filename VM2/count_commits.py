960dc5d7b3b09de217ce4772136e4a408dd9c1cd
alberta007 <abbealpsten@gmail.com>
2025-05-22 23:08:33 +0000

    Now recieving shutdown

--- VM2/count_commits.py
+++ VM2/count_commits.py
@@ -8,8 +8,8 @@ client = pulsar.Client("pulsar://192.168.2.29:6650")
 # Subscribe to the same topic the producer writes to
 consumer = client.subscribe(
     'persistent://public/default/repos-raw',
-    subscription_name='repo-subscription',  # Unique subscription name
-    consumer_type=pulsar.ConsumerType.Shared  # Allows multiple consumers to share load
+    subscription_name='repo-subscription-for-commits', 
+    consumer_type=pulsar.ConsumerType.Exclusive  # Allows multiple consumers to share load
 )
 
 print("\n\nListening for messages on topic: repos-raw\n\n")
@@ -21,6 +21,14 @@ try:
     while True:
         try:
             msg = consumer.receive(timeout_millis=1000)
+            if msg.data() == b"__SHUTDOWN__":
+                print("Received shutdown signal. Exiting.")
+                consumer.acknowledge(msg)
+                try:
+                    count_commits(list_of_repos)
+                except Exception as e:
+                    print("Failed during analysis:", e)
+                break  
             repo = json.loads(msg.data())
             list_of_repos.extend(repo)
             consumer.acknowledge(msg)
@@ -33,12 +41,6 @@ try:
             if 'msg' in locals():
                 consumer.negative_acknowledge(msg)
 
-except KeyboardInterrupt:
-    print("\nStopping consumer.")
-    try:
-        count_commits(list_of_repos)
-    except Exception as e:
-        print("Failed during analysis:", e)
 
 finally:
     consumer.close()
