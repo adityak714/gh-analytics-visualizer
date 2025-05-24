93d4b6cf3c5d474debfdf9d8d824f64f6f427059
albeerta007 <abbealpsten@gmail.com>
2025-05-24 09:22:52 +0000

    Minor stuff

--- VM1/run.py
+++ VM1/run.py
@@ -2,6 +2,9 @@ from datetime import datetime, timedelta, date
 from producer import send_to_pulsar
 from Github_API_fetch import fetch_repo
 import os
+import time
+from Q1 import num_of_languages
+t0 = time.time()
 
 token = os.getenv("GITHUB_TOKEN")
 
@@ -11,10 +14,8 @@ todays_date = datetime.strptime(date.today().strftime("%Y-%m-%d"), "%Y-%m-%d")
 
 
 list_of_repos = []
-print("Hello!")
 # Looping through all dates from starting until todays date.
 while start_date <= todays_date:
-    print("Hello!")
     list_of_repos.extend(fetch_repo(start_date.strftime("%Y-%m-%d"), token))
     start_date += timedelta(days=1)
 
@@ -23,8 +24,16 @@ chunks = [list_of_repos[i:i + 100] for i in range(0, len(list_of_repos), 100)]
 
 for repos in chunks:
     print(f"Sent {len(repos)} repos")
-    send_to_pulsar(repos)
+    send_to_pulsar(repos, False)
 
 
 print(f"Sent a total of {len(list_of_repos)} repos")
+
+t1 = time.time()
+
+
+print(num_of_languages(list_of_repos).most_common(10))
+
+print(f"Total of {t1-t0}s")
+
 # Send the list of repositories to pulsar using send_to_pulsar()
