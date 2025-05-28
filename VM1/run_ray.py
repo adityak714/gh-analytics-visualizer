89d703f3d351a3a4edf46783a025203a8d556235
Aditya Khadkikar <adityakhadkikar@gmail.com>
2025-05-28 21:25:16 +0000

    dockerizing - uploading all involved dockerfiles and docker swarm configs

--- VM1/run_ray.py
+++ VM1/run_ray.py
@@ -2,16 +2,74 @@
 # Send the list of repositories to pulsar using send_to_pulsar() denna kod ger mig 1000 repo per dag,  medans denna inte gör det: from datetime import datetime, timedelta, date
 from datetime import datetime, timedelta, date
 import ray
-from Github_API_fetch import fetch_repo
+# from Github_API_fetch import fetch_repo
 from producer import send_to_pulsar
 import os
 from count_languages import num_of_languages
 import time
 
 # Initialize Ray
-ray.init(
-    address="auto"
-)
+ray.init(address="auto", runtime_env={"working_dir": "."})        # address="ray-head:6379")
+
+import requests
+import time
+import logging
+logging.getLogger("ray").setLevel(logging.WARNING)
+
+
+# Needs to use personal token to access github API
+# Make one with Environment Variables
+
+# Fetches and returns a list of repositories 
+def fetch_repo(date, token):
+
+    headers = {"Authorization": f"token {token}"}
+
+    # Initialize a list to store repositories
+    repos_list = []
+    print(date)
+    # Maximum of 100 per page can be loaded, so we iterate over 10 pages where each page gives us 100 repositories
+    for i in range(1,6):
+
+        # url for Github API for repositories created during specified date
+        # Not archieved and with a maximum of 100 results
+        url=f"https://api.github.com/search/repositories?q=created:{date}+archived:false&per_page=100&page={i}"
+
+        # Make a Get request to the Github API
+        repos = requests.get(url,headers=headers)
+
+        # If maximum request limit has been reached, sleep for 60 seconds
+        if repos.status_code == 403:
+            print("Sleeping...")
+            time.sleep(60)
+            continue
+
+        # If not autheried, break loop
+        if repos.status_code == 401:
+            print("Unautherized")
+            break
+        
+         # Check if response is OK
+        if repos.status_code != 200:
+            print(f"Error: Received status code {repos.status_code} for URL: {url}")
+            break
+        
+        try:
+            repos = repos.json()
+        except requests.exceptions.JSONDecodeError:
+            print(f"Error: Invalid JSON response for URL: {url}")
+            break
+        
+        # Store all repositories in "items"
+        items = repos.get("items",[])
+
+        # If there are no more repositories, we break the loop
+        if not items:
+            break
+
+        # Add the repositories to the list
+        repos_list.extend(items)
+    return repos_list
 
 # Set date range for fetching repositories
 start_date = datetime.strptime("2025-05-19", "%Y-%m-%d")
